94647b2a8540ac4f4ebe25641e52d44e219e1a07
shadowquark <shadowquark@outlook.com>
2022-11-09 02:22:44 +0100

    Letter

--- LetterCombinationsofaPhoneNumber.c
+++ LetterCombinationsofaPhoneNumber.c
@@ -4,9 +4,30 @@
 #include<stdlib.h>
 #include<stdbool.h>
 char **letterCombinations(char *digits, int *returnSize)
+//void letterCombinations(char *digits, int *returnSize)
 {
-	char **x = {"abc", "def"};
-	return x;
+	int lens = strlen(digits);
+	returnSize = malloc(sizeof(int));
+	*returnSize = 0;
+	int a[8] = {3, 3, 3, 3, 3, 4, 3, 4}, pos = 0;
+	char *phone[8] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+	char **out = malloc(sizeof(char));
+	for (int i = 0; i < lens; ++ i)
+	{
+		int key = *(digits + i) - '2';
+		*returnSize += a[key];
+		for (int j = 0; j < strlen(phone[i]); ++ j)
+		{
+			*(out + j) = malloc(sizeof(char) * i);
+			*(*(out + j) + i) = *(phone[i] + j);
+		}
+		printf("%s, ", *out + i);
+		printf("%s\n", *(out + 1) + i);
+	}
+	printf("%d\n", *returnSize);
+	*out = "abc";
+	*(out + 8) = "bcd";
+	printf("%s\n", *(out + 8));
 }
 int main()
 {
@@ -15,12 +36,13 @@ int main()
 	char *s;
 	int *n;
 	fscanf(fin, "%s", s);
-	char **out = letterCombinations(s, n);
-	for (int i = 0; i < *n; ++ i)
-	{
-		printf("%s, ", out[i]);
-		fprintf(fout, "%s, ", out[i]);
-	}
+	letterCombinations(s, n);
+//	char **out = letterCombinations(s, n);
+//	for (int i = 0; i < *n; ++ i)
+//	{
+//		printf("%s, ", out[i]);
+//		fprintf(fout, "%s, ", out[i]);
+//	}
 	return 0;
 }
 
