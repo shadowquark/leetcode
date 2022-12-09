d651d1968cbc9411b9bdd673310a5ea07410581e
shadowquark <shadowquark@outlook.com>
2022-12-09 13:44:59 +0100

    GP

--- GenerateParentheses.py
+++ GenerateParentheses.py
@@ -1,11 +1,24 @@
 from typing import List
 class Solution:
-    def dfs(n):
-        if n = 0:
-            return ')'
+    def dfs(self, l, r, exp):
+        if l == self.n and r == self.n:
+            self.out.append(exp)
+        maxl = self.n - l
+        maxr = min(l - r, self.n - r)
+        if maxl:
+            exp += '('
+            self.dfs(l + 1, r, exp)
+            exp = exp[:-1]
+        if maxr:
+            exp += ')'
+            self.dfs(l, r + 1, exp)
+            exp = exp[:-1]
     def generateParenthesis(self, n: int) -> List[str]:
-        out = Solution.dfs(n)
-        return '(', 
+        self.n = n
+        self.out = []
+        self.dfs(0, 0, '')
+        print(self.out)
+        return self.out
 
 fin = open("oo.xx", "r")
 fout = open("xx.oo", "w")
